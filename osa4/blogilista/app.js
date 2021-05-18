a3b2ea4a76874023ab98895bea2c7bd4f29abf38
Antti Alestalo <alestalo.antti@gmail.com>
2021-05-18 17:34:49 +0300

    implement blog router module

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -3,16 +3,10 @@ const logger = require('./utils/logger')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
 
-const Blog = mongoose.model('Blog', blogSchema)
 
 logger.info('Connecting to MongoDB...')
 mongoose
@@ -27,22 +21,6 @@ mongoose
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
