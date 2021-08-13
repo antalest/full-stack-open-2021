dacfd6222f48ff6436eaba4a97c4233b44fcd11e
Antti Alestalo <alestalo.antti@gmail.com>
2021-08-13 22:28:55 +0300

    add 6.13.-6.14.

--- osa6/redux-anecdotes/src/components/AnecdoteForm.js
+++ osa6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { addAnecdote } from '../reducers/anecdoteReducer'
+import anecdotesService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
 
-  const add = (event) => {
+  const add = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    dispatch(addAnecdote(content))
+    const newAnecdote = await anecdotesService.createNew(content)
+    dispatch(addAnecdote(newAnecdote))
   }
 
   return (
